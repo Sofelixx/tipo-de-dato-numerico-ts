69715a07aa04c9a9fd5a1ec17154e029c75a8b49
sofelix <s18paris@gmail.com>
2019-10-01 20:42:01 -0300

    se compilo el codigo

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+var avengers = 5;
+var villanos = 1;
+var otros = 2;
+if (avengers > villanos) {
+    console.log('Estamos salvados');
+}
+else {
+    console.log('Estamos muertos');
+}
